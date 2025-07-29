4603c17bbdc0f93a112101a31b03b6ae97f2dd62
Christian Munoz Rodriguez <chgeovany.15@gmail.com>
2025-07-29 08:48:43 -0600

    first stable version

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pickerboxd/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   // Example of defining build-time variables (uncomment to use):
